10c7a3b93055d2d695d604fb691ff6c17b0dc6a1
kxen42 <kxen42@users.noreply.github.com>
2023-01-02 20:58:56 -0600

    Fails to Compile

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -9,11 +9,11 @@ public class MoneyTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
 
-        five.times(3);
-        assertEquals(15, five.amount);
+        Dollar product = five.times(3);
+        assertEquals(15, product.amount);
 
     }
 }
