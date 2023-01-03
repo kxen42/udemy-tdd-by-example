06d0f17892d50968731eb2ce1b749ca7e78d1124
kxen42 <kxen42@users.noreply.github.com>
2023-01-02 21:15:50 -0600

    Created:Swiss Franc class

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    void testMultiplication() {
+    void testMultiplicationDollar() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertEquals(new Dollar(10), product);
@@ -20,8 +20,25 @@ public class MoneyTest {
     }
 
     @Test
-    void testEquals() {
+    void testEqualsDollar() {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(8));
+    }
+
+    @Test
+    void testMultiplicationFranc() {
+        Franc five = new Franc(5);
+        Franc product = five.times(2);
+        assertEquals(new Franc(10), product);
+
+        product = five.times(3);
+        assertEquals(new Franc(15), product);
+
+    }
+
+    @Test
+    void testEqualsFranc() {
+        assertEquals(new Franc(5), new Franc(5));
+        assertNotEquals(new Franc(5), new Franc(8));
     }
 }
