cdeae73b7b740b2aee260db8f0e7233dee154486
kxen42 <kxen42@users.noreply.github.com>
2023-01-02 20:45:14 -0600

    Green Test

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -6,9 +6,11 @@ public class Dollar {
     int amount;
 
     public Dollar(int amount) {
+        this.amount = amount;
     }
 
     public void times(int multiplier) {
+        amount = amount * multiplier;
 
     }
 }
